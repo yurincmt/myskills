d3114b6fa906bc9c2907d476e306ade466bd075b
yurincmt <me@example.com>
2025-04-23 10:39:13 -0300

    Style: Lista de habilidades
    * Obter a nova habilidade do usuário e listá-la na tela;

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,22 @@
-import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState();
+  const [skillsList, setSkillsList] = useState([]);
+
+  function handleAddNewSkill() {
+    setSkillsList([...skillsList, newSkill]);
+  }
+  
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
@@ -11,18 +27,30 @@ export function Home() {
         style={styles.textInput}
         placeholder="Nova Habilidade"
         placeholderTextColor='#555'
-
+        onChangeText={setNewSkill}
       />
       
       <TouchableOpacity
         style={styles.button}
-        activeOpacity={0.7}>
+        activeOpacity={0.7}
+        onPress={handleAddNewSkill}>
         <Text style={styles.buttonText}>Adicionar</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, {marginTop: 50}]}>
+      <Text style={[styles.title, {marginVertical: 40}]}>
         Habilidades
       </Text>
+
+      <FlatList
+        data={skillsList}
+        renderItem={(item) => (
+        <View style={styles.skillContainer}>
+          <Text style={styles.skillText}>
+            {item.item}
+          </Text>
+        </View>
+        )}
+      />
     </SafeAreaView>
   )
 }
@@ -58,5 +86,17 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 17,
     fontWeight: 'bold',
+  },
+  skillContainer: {
+    backgroundColor: '#1F1E25',
+    padding: 16,
+    borderRadius: 50,
+    marginVertical: 5,
+  },
+  skillText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    fontSize: 20,
   }
 })
