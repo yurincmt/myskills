142669deade680c859ab6a11f49ec1f36e7a9eef
yurincmt <me@example.com>
2025-04-26 13:50:25 -0300

    fix: icones branco na statusbar

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Home } from "./src/Home";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar barStyle="light-content"/>
+      <Home />
+    </>
   )
 }
